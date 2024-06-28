de07042f6ad54efa0500a033cd55d159e95d838e
Harsh Dhama <dhamaharsh9@gmail.com>
2024-06-28 11:52:16 +0530

    about component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
     <div className='container'>
       <Title subTitle='Our Programs' title='What we offer'/>
       <Programs/>
+      <About/>
     </div>
     </div>
   )
--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -3,9 +3,9 @@ import "./Programs.css";
 import program_1 from '../../assets/program-1.png'
 import program_2 from '../../assets/program-2.png'
 import program_3 from '../../assets/program-3.png'
-import program_icon_1 from '../../assets/program_icon_1.png'
-import program_icon_2 from '../../assets/program_icon_2.png'
-import program_icon_3 from '../../assets/program_icon_3.png'
+import program_icon_1 from '../../assets/program-icon-1.png'
+import program_icon_2 from '../../assets/program-icon-2.png'
+import program_icon_3 from '../../assets/program-icon-3.png'
 
 const Programs = () => {
     return (
