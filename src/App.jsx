e0c100377fec51191668d8e51139b4ae4299a878
Harsh Dhama <dhamaharsh9@gmail.com>
2024-06-29 17:05:47 +0530

    Campus Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
 
 const App = () => {
   return (
@@ -15,6 +16,8 @@ const App = () => {
       <Programs/>
       <About/>
       <Title subTitle='Gallery' title='Campus Photos'/>
+      <Campus/>
+      <Title subTitle='Testimonials' title='What Student Says'/>
     </div>
     </div>
   )
