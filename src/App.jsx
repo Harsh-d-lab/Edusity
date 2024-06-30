2e696c765e1387cdcd1e0a65dada03c9dd2d2879
Harsh Dhama <dhamaharsh9@gmail.com>
2024-06-30 18:49:45 +0530

    Testimonial Comp. Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
 
 const App = () => {
   return (
@@ -18,6 +19,8 @@ const App = () => {
       <Title subTitle='Gallery' title='Campus Photos'/>
       <Campus/>
       <Title subTitle='Testimonials' title='What Student Says'/>
+      <Testimonials/>
+      <Title subTitle='Contact Us' title='Get in Touch'/>
     </div>
     </div>
   )
