6f8db3d28c5d8840eef1b94f84724ede8b631ab5
Harsh Dhama <dhamaharsh9@gmail.com>
2024-07-02 19:30:32 +0530

    react-scroll added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll';
+import Campus from './../Campus/Campus';
+import Testimonials from './../Testimonials/Testimonials';
+import Contact from './../Contact/Contact';
 
 const Navbar = () => {
 
@@ -20,24 +24,13 @@ const Navbar = () => {
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
       <img src={logo} alt='' className='logo'/>
       <ul>
-        <li>
-          <a href='#'>Home</a>
+        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link>
         </li>
-        <li>
-          <a href='#'>Program</a>
-        </li>
-        <li>
-          <a href='#'>About Us</a>
-        </li>
-        <li>
-          <a href='#'>Campus</a>
-        </li>
-        <li>
-          <a href='#'>Testimonials</a>
-        </li>
-        <li>
-          <button className='btn'>Contact Us</button><a href='#'></a>
-        </li>
+        <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+        <li><Link to='about' smooth={true} offset={-150} duration={500}>About</Link></li>
+        <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+        <li><Link to='contact' smooth={true} offset={0} duration={-260} className='btn'>Contact Us</Link></li>
       </ul>
     </nav>
   )
